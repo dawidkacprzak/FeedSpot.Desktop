bd691f5c0d63e4f447af58ed2c4b3f07bc51779d
capthapl <dawidkacprzak@icloud.com>
2020-01-03 09:36:03 +0100

    Created first design in loading window

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+let loadingWin;
+function createWindow() {
+  loadingWin = new BrowserWindow({
+    width: 400,
+    height: 300,
+    frame: false,
+    hasShadow: false,
+    alwaysOnTop: true,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  loadingWin.webContents.on("devtools-opened", () => {
+    win.webContents.closeDevTools();
+  });
+    
+  loadingWin.loadFile("./pages/Loading.html");
+}
+
+app.on("ready", createWindow);
