910491453aeb61492b350d18f3abf879df2df8e3
capthapl <dawidkacprzak@icloud.com>
2020-01-04 21:51:07 +0100

    Created league client sign in connector

--- src/PageSpecific/Main.js
+++ src/PageSpecific/Main.js
@@ -1,29 +1,43 @@
 const { ipcRenderer, ipcMain } = require("electron");
-
+const LCUConnector = require("lcu-connector");
 let minimalizeButton;
 let closeButton;
 let fullscreenButton;
+let pleaseOpenLolLabel;
+const connector = new LCUConnector();
+
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 window.onload = () => {
-    minimalizeButton = document.getElementById("app-func-minimalize");
-    closeButton = document.getElementById("app-func-close");
-    fullscreenButton = document.getElementById("app-func-fullscreen");
+  minimalizeButton = document.getElementById("app-func-minimalize");
+  closeButton = document.getElementById("app-func-close");
+  fullscreenButton = document.getElementById("app-func-fullscreen");
+  pleaseOpenLolLabel = document.getElementById("waiting-for-league");
+  ipcRenderer.send("getFullscreenIcon");
+
+  minimalizeButton.onclick = () => {
+    ipcRenderer.send("minimalize");
+  };
+
+  closeButton.onclick = () => {
+    ipcRenderer.send("close");
+  };
+
+  fullscreenButton.onclick = () => {
+    ipcRenderer.send("changeFullscreenState");
     ipcRenderer.send("getFullscreenIcon");
+  };
 
-    minimalizeButton.onclick = () => {
-        ipcRenderer.send("minimalize");
-    }
+  ipcRenderer.on("getFullscreenIcon", (event, args) => {
+    fullscreenButton.src = "../content/images/" + args.icon;
+  });
 
-    closeButton.onclick = () => {
-        ipcRenderer.send("close");
-    }
-
-    fullscreenButton.onclick = () => {
-        ipcRenderer.send("changeFullscreenState");
-        ipcRenderer.send("getFullscreenIcon");
-    }
-
-    ipcRenderer.on("getFullscreenIcon", (event,args) => {
-        fullscreenButton.src = "../content/images/"+args.icon
-    })
-}
+  sleep(1000).then(() => {
+    connector.on("connect", data => {
+      pleaseOpenLolLabel.style.display = "none";
+    });
+    connector.start();
+  });
+};
